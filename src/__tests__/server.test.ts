12771214c7d430a26506a06b35845552c02d501e
Luis Roque <luisrqvgdev@hotmail.com>
2026-03-22 00:17:18 -0700

    Se completa la documentación de la API según la sección 22. fixes #5

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,21 +1,9 @@
-import request from 'supertest'
-import server, { connectDB } from '../server.js'
+import { connectDB } from '../server.js'
 import db from '../config/db.js'
 import { jest } from '@jest/globals'
 
-describe('GET /api', () => {
-    it('should send back a json response', async () => {
-        const res = await request(server).get('/api')        
-        expect(res.status).toBe(200)
-        expect(res.header['content-type']).toMatch(/json/) // Verifica si existe en el string json > application/json : Existe
-        expect(res.body.msg).toBe('Desde API')
-
-        expect(res.status).not.toBe(404)
-        expect(res.body.msg).not.toBe('desde api')
-    })
-})
-
 jest.mock('../config/db.js')
+
 describe('connectDB', () => {
     it('should handle database connection error', async() => {
         jest.spyOn(db, 'authenticate')
