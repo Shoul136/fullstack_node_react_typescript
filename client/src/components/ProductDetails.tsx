fe43fd6e7d9864295f531c15ec81d599d317989b
Luis Roque <luisrqvgdev@hotmail.com>
2026-03-30 20:46:36 -0700

    feat: Funcionalidad eliminar un producto implementada correctamente. #7

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,34 +1,60 @@
+import { deleteProduct } from "../services/ProductService";
 import type { Product } from "../types"
 import { formatCurrency } from "../utils"
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate, type ActionFunctionArgs, redirect} from "react-router-dom";
 
 type ProductDetailsProps = {
     product: Product
 }
 
-export default function ProductDetails({product} : ProductDetailsProps) {
-    
+export async function action({params} : ActionFunctionArgs){   
+    if(params.id !== undefined)
+    {
+        await deleteProduct(+params.id);
+        return redirect('/')
+    }        
+}
+
+export default function ProductDetails({ product }: ProductDetailsProps) {
+
     const navigate = useNavigate();
     const isAvailable = product.availability;
-    
+
     return (
         <tr className="border-b border-gray-300">
             <td className="p-3 text-lg text-gray-800">
                 {product.name}
             </td>
             <td className="p-3 text-lg text-gray-800">
-                { formatCurrency(product.price) }
+                {formatCurrency(product.price)}
             </td>
             <td className="p-3 text-lg text-gray-800">
                 {isAvailable ? 'Disponible' : 'No Disponible'}
             </td>
             <td className="p-3 text-lg text-gray-800 ">
                 <div className="flex gap-2 items-center">
-                    <button 
-                    onClick={() => navigate(`/productos/${product.id}/editar`)} 
-                    className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center">
+                    <button
+                        onClick={() => navigate(`/productos/${product.id}/editar`)}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center">
                         Editar
                     </button>
+                    <Form 
+                        className="w-full"
+                        method="POST"
+                        action={`productos/${product.id}/eliminar`}
+                        onSubmit={(e) => {
+                            if(!confirm('¿Eliminar?'))
+                            {
+                                e.preventDefault()
+                            }
+                        }}  
+                    >
+                        <input 
+                            type='submit'                            
+                            value='Eliminar'
+                            className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                        />
+                    </Form>
                 </div>
             </td>
         </tr>
