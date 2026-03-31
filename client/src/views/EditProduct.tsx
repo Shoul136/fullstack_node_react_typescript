abf17b09b32b3c628ca9b1a54395c9f94bda534b
Luis Roque <luisrqvgdev@hotmail.com>
2026-03-30 20:24:24 -0700

    feat: Se creo el componente ProductForm para limpiar la vista. #7

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -1,7 +1,8 @@
 import { Link, Form, useActionData, type ActionFunctionArgs, redirect, type LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
-import { addProduct, getProductById, updateProduct } from "../services/ProductService";
+import { getProductById, updateProduct } from "../services/ProductService";
 import type { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
 export async function loader({ params }: LoaderFunctionArgs) {
     if (params.id !== undefined) {
@@ -61,34 +62,8 @@ export default function EditProduct() {
                 method="POST"
             >
 
-                <div className="mb-4">
-                    <label
-                        className="text-gray-800"
-                        htmlFor="name"
-                    >Nombre Producto:</label>
-                    <input
-                        id="name"
-                        type="text"
-                        className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Nombre del Producto"
-                        name="name"
-                        defaultValue={product.name}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label
-                        className="text-gray-800"
-                        htmlFor="price"
-                    >Precio:</label>
-                    <input
-                        id="price"
-                        type="number"
-                        className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Precio Producto. ej. 200, 300"
-                        name="price"
-                        defaultValue={product.price}
-                    />
-                </div>
+                <ProductForm product={product} />
+
                 <div className="mb-4">
                     <label
                         className="text-gray-800"
@@ -108,7 +83,7 @@ export default function EditProduct() {
                 <input
                     type="submit"
                     className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-                    value="Actualizar Producto"
+                    value="Guardar Cambios"
                 />
             </Form>
         </>
