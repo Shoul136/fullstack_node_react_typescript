1f98da58f7f081c110b70f702c487e5c9c04f870
Luis Roque <luisrqvgdev@hotmail.com>
2026-03-30 20:02:35 -0700

    feat: Editar Producto terminado, validaciones de tipos de datos usando valibot, configuraciones en router, etc. #7

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,11 +1,14 @@
 import type { Product } from "../types"
 import { formatCurrency } from "../utils"
+import { useNavigate } from "react-router-dom";
 
 type ProductDetailsProps = {
     product: Product
 }
 
 export default function ProductDetails({product} : ProductDetailsProps) {
+    
+    const navigate = useNavigate();
     const isAvailable = product.availability;
     
     return (
@@ -20,7 +23,13 @@ export default function ProductDetails({product} : ProductDetailsProps) {
                 {isAvailable ? 'Disponible' : 'No Disponible'}
             </td>
             <td className="p-3 text-lg text-gray-800 ">
-
+                <div className="flex gap-2 items-center">
+                    <button 
+                    onClick={() => navigate(`/productos/${product.id}/editar`)} 
+                    className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center">
+                        Editar
+                    </button>
+                </div>
             </td>
         </tr>
     )
